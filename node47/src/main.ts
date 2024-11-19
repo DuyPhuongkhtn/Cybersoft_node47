5201ae48cf15307b6e29a9a9eb42db290e3e24fe
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2024-11-19 16:50:53 +0700

    Push code nestjs

---
+++ node47/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // add validation input
+  app.useGlobalPipes(new ValidationPipe());
+
+  // config swagger UI
+  const configSwagger = new DocumentBuilder()
+  .setTitle("API Youtube mini")
+  .setDescription("Danh sách API youtube mini")
+  .setVersion("1.0")
+  .build(); // builder pattern
+
+  const swagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup("swagger", app, swagger);
+
+  await app.listen(process.env.PORT ?? 8080);
+}
+bootstrap();
+
+// javascript và typescript
+// support code class component và  functional component
---
+++ node47/src/user/user.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query, Headers } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Request } from 'express';
+import { ApiHeader, ApiQuery } from '@nestjs/swagger';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get("/list-user/:id")
+  @ApiQuery({name: "keyword", required: false, type: String}) // config query trên swagger
+  @ApiHeader({name: "token", required: false}) // config header trên swagger
+  findAll(
+    @Param('id') id: string, // lấy param theo kiểu nestjs
+    @Query("keyword") keyword: string, // lấy query theo kiểu nestjs
+    @Headers("token") token: string, // lấy info header theo kiểu nestjs
+    @Req() req: Request
+  ): any {
+    let id1 = req.params.id; // lấy param theo kiểu express
+    let keyword1 = req.query.keyword; // lấy query theo kiểu express
+    let token1 = req.headers["token"]; // lấy info header theo kiểu express
+
+    return {id, id1, keyword, keyword1, token, token1};
+    // return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
